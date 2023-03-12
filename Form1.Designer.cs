679c8d9a16f44c090e5c8d9ee7470ae92c09e009
Danilo <Danilo@DANILO-PC>
2023-03-12 22:51:03 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace kalkulator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.rbr = new System.Windows.Forms.RadioButton();
+            this.rbk = new System.Windows.Forms.RadioButton();
+            this.rbd = new System.Windows.Forms.RadioButton();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.ulaz = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // rbr
+            // 
+            this.rbr.AutoSize = true;
+            this.rbr.Location = new System.Drawing.Point(34, 13);
+            this.rbr.Margin = new System.Windows.Forms.Padding(4);
+            this.rbr.Name = "rbr";
+            this.rbr.Size = new System.Drawing.Size(111, 23);
+            this.rbr.TabIndex = 0;
+            this.rbr.TabStop = true;
+            this.rbr.Text = "Rimski brojevi";
+            this.rbr.UseVisualStyleBackColor = true;
+            // 
+            // rbk
+            // 
+            this.rbk.AutoSize = true;
+            this.rbk.Location = new System.Drawing.Point(153, 13);
+            this.rbk.Margin = new System.Windows.Forms.Padding(4);
+            this.rbk.Name = "rbk";
+            this.rbk.Size = new System.Drawing.Size(142, 23);
+            this.rbk.TabIndex = 1;
+            this.rbk.TabStop = true;
+            this.rbk.Text = "Kompleksni brojevi";
+            this.rbk.UseVisualStyleBackColor = true;
+            // 
+            // rbd
+            // 
+            this.rbd.AutoSize = true;
+            this.rbd.Location = new System.Drawing.Point(303, 13);
+            this.rbd.Margin = new System.Windows.Forms.Padding(4);
+            this.rbd.Name = "rbd";
+            this.rbd.Size = new System.Drawing.Size(101, 23);
+            this.rbd.TabIndex = 2;
+            this.rbd.TabStop = true;
+            this.rbd.Text = "Dugi brojevi";
+            this.rbd.UseVisualStyleBackColor = true;
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(34, 405);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(66, 70);
+            this.btn1.TabIndex = 3;
+            this.btn1.Text = "+";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(110, 405);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(66, 70);
+            this.btn2.TabIndex = 4;
+            this.btn2.Text = "-";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(186, 405);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(66, 70);
+            this.btn3.TabIndex = 5;
+            this.btn3.Text = "*";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Location = new System.Drawing.Point(262, 405);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(66, 70);
+            this.btn4.TabIndex = 6;
+            this.btn4.Text = "/";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Location = new System.Drawing.Point(338, 405);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(66, 70);
+            this.btn5.TabIndex = 7;
+            this.btn5.Text = "=";
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // ulaz
+            // 
+            this.ulaz.Location = new System.Drawing.Point(34, 43);
+            this.ulaz.Multiline = true;
+            this.ulaz.Name = "ulaz";
+            this.ulaz.Size = new System.Drawing.Size(370, 356);
+            this.ulaz.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Desktop;
+            this.ClientSize = new System.Drawing.Size(444, 498);
+            this.Controls.Add(this.ulaz);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.rbd);
+            this.Controls.Add(this.rbk);
+            this.Controls.Add(this.rbr);
+            this.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Kalkulator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton rbr;
+        private System.Windows.Forms.RadioButton rbk;
+        private System.Windows.Forms.RadioButton rbd;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.TextBox ulaz;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,576 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Numerics;
+
+namespace kalkulator
+{
+    public class Kompleksni
+    {
+        public int realni;
+        public int imaginarni;
+
+        public Kompleksni() { }
+
+        public Kompleksni(int realni, int imaginarni)
+        {
+            this.realni = realni;
+            this.imaginarni = imaginarni;
+        }
+
+        public void konvert(string s)
+        {
+            char[] znakovi = { '+', '-', '*', '/' };
+
+            string[] niz = s.Split(znakovi, StringSplitOptions.RemoveEmptyEntries);
+
+            if (Int32.TryParse(niz[0], out realni))
+            {
+                niz[1] = niz[1].Remove(niz[1].Length - 1);
+                if (niz[1] == "")
+                {
+                    imaginarni = 1;
+                }
+                else
+                {
+                    imaginarni = Int32.Parse(niz[1]);
+                }
+                if (s.Contains('-') && (s.IndexOf('-') == 0))
+                {
+                    realni *= -1;
+                    s = s.Remove(0, 1);
+
+                    if (s.Contains('-'))
+                    {
+                        imaginarni *= -1;
+                    }
+                }
+                else if(s.Contains('-'))
+                {
+                    imaginarni *= -1;
+                }
+            }
+            else
+            {
+                realni = 0;
+                niz[0] = niz[0].Remove(niz[0].Length - 1);
+                if (niz[0] == "")
+                {
+                    imaginarni = 1;
+                }
+                else
+                {
+                    imaginarni = Int32.Parse(niz[0]);
+                }
+                if(s.Contains('-'))
+                {
+                    imaginarni *= -1;
+                }
+            }
+        }
+
+        public static Kompleksni operator +(Kompleksni k1, Kompleksni k2)
+        {
+            return new Kompleksni(k1.realni + k2.realni, k1.imaginarni + k2.imaginarni);
+        }
+
+        public static Kompleksni operator -(Kompleksni k1, Kompleksni k2)
+        {
+            return new Kompleksni(k1.realni - k2.realni, k1.imaginarni - k2.imaginarni);
+        }
+
+        public static Kompleksni operator *(Kompleksni k1, Kompleksni k2)
+        {
+            return new Kompleksni(k1.realni * k2.realni - k1.imaginarni * k2.imaginarni, k1.imaginarni * k2.realni + k1.realni * k2.imaginarni);
+        }
+
+        public static Kompleksni operator /(Kompleksni k1, Kompleksni k2)
+        {
+            Kompleksni pom = new Kompleksni();
+            k2.imaginarni *= -1;
+            pom = k1 * k2;
+            k2.imaginarni *= -1;
+            pom.realni /= k2.realni + k2.imaginarni;
+            pom.imaginarni /= k2.realni + k2.imaginarni;
+            return pom;
+        }
+
+        public string ispis()
+        {
+            string s = Convert.ToString(this.realni);
+            if(this.imaginarni >= 0)
+            {
+                s += "+";
+            }
+
+            s += Convert.ToString(this.imaginarni);
+            s += "i";
+
+            return s;
+        }
+    }
+
+    public class Rimski
+    {
+        public int prvi;
+
+        public Rimski() { }
+
+        public Rimski(int prvi)
+        {
+            this.prvi = prvi;
+        }
+
+        public void konvert(string s)
+        {
+            char[] niz = s.ToCharArray();
+            int[] pom = new int[niz.Length];
+            for(int i = 0; i < niz.Length; i++)
+            {
+                switch(niz[i])
+                {
+                    case 'I':
+                        pom[i] = 1;
+                        break;
+                    case 'V':
+                        pom[i] = 5;
+                        break;
+                    case 'X':
+                        pom[i] = 10;
+                        break;
+                    case 'L':
+                        pom[i] = 50;
+                        break;
+                    case 'C':
+                        pom[i] = 100;
+                        break;
+                    case 'D':
+                        pom[i] = 500;
+                        break;
+                    case 'M':
+                        pom[i] = 1000;
+                        break;
+                }
+            }
+
+            for(int i = 0; i < pom.Length - 1; i++)
+            {
+                if (pom[i + 1] > pom[i])
+                {
+                    prvi -= pom[i];
+                }
+                else
+                {
+                    prvi += pom[i];
+                }
+            }
+            prvi += pom[pom.Length - 1];
+        }
+
+        public static Rimski operator +(Rimski r1, Rimski r2)
+        {
+            return new Rimski(r1.prvi + r2.prvi);
+        }
+
+        public static Rimski operator -(Rimski r1, Rimski r2)
+        {
+            return new Rimski(r1.prvi - r2.prvi);
+        }
+
+        public static Rimski operator *(Rimski r1, Rimski r2)
+        {
+            return new Rimski(r1.prvi * r2.prvi);
+        }
+
+        public static Rimski operator /(Rimski r1, Rimski r2)
+        {
+            return new Rimski(r1.prvi / r2.prvi);
+        }
+
+        public string ispis()
+        {
+            if ((prvi > 3999) || (prvi < 0))
+            {
+                return "Broj nije moguce napisati rimskim ciframa";
+            }
+            else
+            {
+                char[] niz = new char[50];
+                int br = 0;
+                int k = 2;
+                int mod = 1000;
+
+                while (prvi > 0)
+                {
+                    if((prvi / mod) > 0)
+                    {
+                        switch (mod)
+                        {
+                            case 1000:
+                                niz[br] = 'M';
+                                break;
+                            case 500:
+                                niz[br] = 'D';
+                                break;
+                            case 100:
+                                niz[br] = 'C';
+                                break;
+                            case 50:
+                                niz[br] = 'L';
+                                break;
+                            case 10:
+                                niz[br] = 'X';
+                                break;
+                            case 5:
+                                niz[br] = 'V';
+                                break;
+                            case 1:
+                                niz[br] = 'I';
+                                break;
+                        }
+
+                        prvi -= mod;
+
+                        br++;
+                    }
+                    else
+                    {
+                        mod /= k;
+                        if(k == 2)
+                        {
+                            k = 5;
+                        }
+                        else
+                        {
+                            k = 2;
+                        }
+                    }
+                }
+
+                int pon = 0;
+                char c = 'M';
+                string s = "";
+
+                for(int i = 0; i < br; i++)
+                {
+                    if (niz[i] == c)
+                    {
+                        pon++;
+                    }
+                    else
+                    {
+                        c = niz[i];
+                        pon = 1;
+                    }
+
+                    if(pon == 4)
+                    {
+                        pon = 0;
+                        switch(c)
+                        {
+                            case 'C':
+                                if (i > 3)
+                                {
+                                    if (niz[i - 4] == 'D')
+                                    {
+                                        s = s.Remove(s.Length - 4);
+                                        s += "CM";
+                                    }
+                                    else
+                                    {
+                                        s = s.Remove(s.Length - 3);
+                                        s += "CD";
+                                    }
+                                }
+                                else
+                                {
+                                    s = "";
+                                    s += "CD";
+                                }
+                                break;
+                            case 'X':
+                                if (i > 3)
+                                {
+                                    if (niz[i - 4] == 'L')
+                                    {
+                                        s = s.Remove(s.Length - 4);
+                                        s += "XC";
+                                    }
+                                    else
+                                    {
+                                        s = s.Remove(s.Length - 3);
+                                        s += "XL";
+                                    }
+                                }
+                                else
+                                {
+                                    s = "";
+                                    s += "XL";
+                                }
+                                break;
+                            case 'I':
+                                if(i > 3)
+                                {
+                                    if (niz[i - 4] == 'V')
+                                    {
+                                        s = s.Remove(s.Length - 4);
+                                        s += "IX";
+                                    }
+                                    else
+                                    {
+                                        s = s.Remove(s.Length - 3);
+                                        s += "IV";
+                                    }
+                                }
+                                else
+                                {
+                                    s = "";
+                                    s += "IV";
+                                }
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        s += Convert.ToString(niz[i]);
+                    }
+                }
+                return s;
+            }
+        }
+    }
+
+    class Dugi
+    {
+        BigInteger broj;
+        int zarez;
+
+        public Dugi() { }
+
+        public Dugi(BigInteger broj, int zarez)
+        {
+            this.broj = broj;
+            this.zarez = zarez;
+        }
+
+        public void konvert(string s)
+        {
+            if(s.Contains(','))
+            {
+                zarez = s.IndexOf(',');
+            }
+            else if (s.Contains('.'))
+            {
+                zarez = s.IndexOf('.');
+            }
+
+            s = s.Remove(zarez, 1);
+
+            broj = BigInteger.Parse(s);
+
+            zarez = s.Length - zarez;
+        }
+
+        public static Dugi operator +(Dugi d1, Dugi d2)
+        {
+            if(d1.broj < d2.broj)
+            {
+                d1.broj *= (BigInteger)Math.Pow(10, (d1.zarez - d2.zarez));
+                d1.zarez = d2.zarez;
+            }
+            else
+            {
+                d2.broj *= (BigInteger)Math.Pow(10, (d2.zarez - d1.zarez));
+            }
+
+            return new Dugi(d1.broj + d2.broj, d1.zarez);
+        }
+
+        public static Dugi operator -(Dugi d1, Dugi d2)
+        {
+            if (d1.broj < d2.broj)
+            {
+                d1.broj *= 10 ^ (d2.zarez - d1.zarez);
+                d1.zarez = d2.zarez;
+            }
+            else
+            {
+                d2.broj *= 10 ^ (d1.zarez - d2.zarez);
+            }
+
+            return new Dugi(d1.broj - d2.broj, d1.zarez);
+
+        }
+
+        public static Dugi operator *(Dugi d1, Dugi d2)
+        {
+            return new Dugi(d1.broj * d2.broj, d1.zarez + d2.zarez + 1);
+        }
+
+        public static Dugi operator /(Dugi d1, Dugi d2)
+        {
+            if(d1.broj > d2.broj)
+            {
+                d1.broj *= (BigInteger)d2.zarez;
+                d2.broj *= (BigInteger)d2.zarez;
+            }
+            else
+            {
+                d1.broj *= (BigInteger)d1.zarez;
+                d2.broj *= (BigInteger)d1.zarez;
+            }
+
+            return new Dugi(d1.broj / d2.broj, 0);
+
+        }
+
+        public string ispis()
+        {
+            if(Convert.ToString(broj).Length <= zarez)
+            {
+                return Convert.ToString(broj).Insert(0, "0,");
+            }
+            return Convert.ToString(broj).Insert(Convert.ToString(broj).Length - zarez, ",");
+        }
+    }
+
+    public partial class Form1 : Form
+    {
+        private string prvi;
+        private string drugi;
+        private string znak;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            znak = "+";
+            prvi = ulaz.Text;
+            ulaz.Text = "";
+            ulaz.Focus();
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            znak = "-";
+            prvi = ulaz.Text;
+            ulaz.Text = "";
+            ulaz.Focus();
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            znak = "*";
+            prvi = ulaz.Text;
+            ulaz.Text = "";
+            ulaz.Focus();
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            znak = "/";
+            prvi = ulaz.Text;
+            ulaz.Text = "";
+            ulaz.Focus();
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            drugi = ulaz.Text;
+
+            if(rbr.Checked)
+            {
+                Rimski rez = new Rimski();
+                Rimski prvi_cinilac = new Rimski();
+                Rimski drugi_cinilac = new Rimski();
+
+                prvi_cinilac.konvert(prvi);
+                drugi_cinilac.konvert(drugi);
+
+
+                switch (znak)
+                {
+                    case "+":
+                        rez = prvi_cinilac + drugi_cinilac;
+                        break;
+                    case "-":
+                        rez = prvi_cinilac - drugi_cinilac;
+                        break;
+                    case "*":
+                        rez = prvi_cinilac * drugi_cinilac;
+                        break;
+                    case "/":
+                        rez = prvi_cinilac / drugi_cinilac;
+                        break;
+                }
+
+                ulaz.Text = rez.ispis();
+            }
+            else if(rbk.Checked)
+            {
+                Kompleksni rez = new Kompleksni();
+                Kompleksni prvi_cinilac = new Kompleksni();
+                Kompleksni drugi_cinilac = new Kompleksni();
+
+                prvi_cinilac.konvert(prvi);
+                drugi_cinilac.konvert(drugi);
+
+                switch(znak)
+                {
+                    case "+":
+                        rez = prvi_cinilac + drugi_cinilac;
+                        break;
+                    case "-":
+                        rez = prvi_cinilac - drugi_cinilac;
+                        break;
+                    case "*":
+                        rez = prvi_cinilac * drugi_cinilac;
+                        break;
+                    case "/":
+                        rez = prvi_cinilac / drugi_cinilac;
+                        break;
+                }
+
+                ulaz.Text = rez.ispis();
+            }
+            else if(rbd.Checked)
+            {
+                Dugi prvi_cinilac = new Dugi();
+                Dugi drugi_cinilac = new Dugi();
+                Dugi rez = new Dugi();
+
+                prvi_cinilac.konvert(prvi);
+                drugi_cinilac.konvert(drugi);
+
+                switch (znak)
+                {
+                    case "+":
+                        rez = prvi_cinilac + drugi_cinilac;
+                        break;
+                    case "-":
+                        rez = prvi_cinilac - drugi_cinilac;
+                        break;
+                    case "*":
+                        rez = prvi_cinilac * drugi_cinilac;
+                        break;
+                    case "/":
+                        rez = prvi_cinilac / drugi_cinilac;
+                        break;
+                }
+
+                ulaz.Text = rez.ispis();
+            }
+
+            prvi = "";
+            drugi = "";
+        }
+    }
+}
